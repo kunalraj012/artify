d091f535e37b8da0302b9b872cbd06f457f88b9c
gaurav <shukla5bhaiya@gmail.com>
2023-12-16 19:49:17 +0530

    footer by gaurav shukla

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,26 @@
-import res from "express/lib/response";
-// import { Result } from "postcss";
-import { createApi } from "unsplash-js";
-// import { photos } from "unsplash-js/dist/internals";
+// import res from "express/lib/response";
+// // import { Result } from "postcss";
+// import { createApi } from "unsplash-js";
+// // import { photos } from "unsplash-js/dist/internals";
 
-const main = document.querySelectorAll('#main');
-const unsplash = createApi({
-  accessKey:'000DvTexIa5o0rfXHeKlY66ZdmBh9xqnFNIlOqmmULU',
-});
+// const main = document.querySelectorAll('#main');
+// const unsplash = createApi({
+//   accessKey:'000DvTexIa5o0rfXHeKlY66ZdmBh9xqnFNIlOqmmULU',
+// });
 
-unsplash.search.getPhotos({
-  query:'Classic Art',
-  page:1,
-  perPage:12,
-  orientation:'portrait'
-}).then(result => {
-  if(result.type === 'success'){
-    const photos = result.response.results;
-    const getUrls = photos.map((i) => {
-      return `<img src = "${i.urls.small}" />`;
-    });
-    main.innerhtml = getUrls.join('');
+// unsplash.search.getPhotos({
+//   query:'Classic Art',
+//   page:1,
+//   perPage:12,
+//   orientation:'portrait'
+// }).then(result => {
+//   if(result.type === 'success'){
+//     const photos = result.response.results;
+//     const getUrls = photos.map((i) => {
+//       return `<img src = "${i.urls.small}" />`;
+//     });
+//     main.innerhtml = getUrls.join('');
 
-  }
-});
+//   }
+// });
 
