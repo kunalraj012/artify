a7f223594bc83d7655cc9c5d6598c1fb83eeeb4c
shubham kumar <90451740+starchastag@users.noreply.github.com>
2023-12-16 19:48:31 +0530

    js for api content

--- src/js/index.js
+++ src/js/index.js
@@ -1,26 +1,21 @@
-// import res from "express/lib/response";
-// import { Result } from "postcss";
-// import { createApi } from "unsplash-js";
-// import { photos } from "unsplash-js/dist/internals";
+import { createApi } from "unsplash-js";
 
-// const main = document.querySelectorAll('#main');
-// const unsplash = createApi({
-//   accessKey:'000DvTexIa5o0rfXHeKlY66ZdmBh9xqnFNIlOqmmULU',
-// });
+const maincontent = document.querySelectorAll('#maincontent');
+const unsplash = createApi({
+  accessKey:"000DvTexIa5o0rfXHeKlY66ZdmBh9xqnFNIlOqmmULU",
+});
 
-// unsplash.search.getPhotos({
-//   query:'Classic Art',
-//   page:1,
-//   perPage:12,
-//   orientation:'portrait'
-// }).then(result => {
-//   if(result.type === 'success'){
-//     const photos = result.response.results;
-//     const getUrls = photos.map((i) => {
-//       return `<img src = "${i.urls.small}" />`;
-//     });
-//     main.innerhtml = getUrls.join('');
-
-//   }
-// });
-
+unsplash.search.getPhotos({
+  query:'Classic Art',
+  page:1,
+  perPage:12,
+  orientation:'portrait',
+}).then(result => {
+  if(result.type === 'success'){
+    const photos = result.response.results;
+    const getUrls = photos.map((i) => {
+      return `<img src="${i.urls.small}" />`;
+    });
+    maincontent.innerHTMl = getUrls.join('');
+  }
+});
