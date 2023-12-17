4291d9bcde23ff8fd2473333ef8255260a213b9b
shubham kumar <90451740+starchastag@users.noreply.github.com>
2023-12-17 11:13:01 +0530

    javascript some added

--- src/js/index.js
+++ src/js/index.js
@@ -1,20 +1,21 @@
 import { createApi } from "unsplash-js";
 
-const maincontent = document.querySelectorAll('#maincontent');
+const maincontent = document.querySelector('#maincontent');
 const unsplash = createApi({
-  accessKey:"000DvTexIa5o0rfXHeKlY66ZdmBh9xqnFNIlOqmmULU",
+  accessKey:'000DvTexIa5o0rfXHeKlY66ZdmBh9xqnFNIlOqmmULU',
 });
 unsplash.search.getPhotos({
   query:'Classic Art',
   page:1,
-  perPage:12,
+  perPage:50,
   orientation:'portrait',
 }).then(result => {
   if(result.type === 'success'){
     const photos = result.response.results;
+    console.log(photos)
     const getUrls = photos.map((i) => {
       return `<img src="${i.urls.small}" />`;
     });
-    maincontent.innerHTMl = getUrls.join('');
+    maincontent.innerHTML = getUrls.join('');
   }
 });
