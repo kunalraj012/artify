d0a466b5d0e4bb5085fe3e5a191ed0e375843ecd
kunalraj012 <kunalraj0631@gmail.com>
2023-12-17 22:02:05 +0530

    updated all files

--- src/js/index.js
+++ src/js/index.js
@@ -1,14 +1,23 @@
+import {createApi} from 'unsplash-js'
 
-import { createApi } from "unsplash-js";
+const accessKey = "vQMsVs2qIFA-STFTyhRgnYCOUL2UauZqwLotPJR6z8Q";
 
-const maincontent = document.querySelector('.container');
+const searchForm = document.getElementById("search-form");
+const searchBox = document.getElementById("search-box");
+const searchResult = document.getElementById("search-result");
+const showMoreBtn = document.getElementById("show-more-btn"); 
+const defaultcontent = document.getElementById("defaultcontent");
+
+
+
+
 const unsplash = createApi({
   accessKey: '000DvTexIa5o0rfXHeKlY66ZdmBh9xqnFNIlOqmmULU',
 });
 unsplash.search.getPhotos({
-  query:'All Types',
+  query:'Classic Art',
   page:1,
-  perPage:100,
+  perPage:20,
   orientation:'portrait',
 }).then(result => {
   if (result.type === 'success') {
@@ -18,8 +27,58 @@ unsplash.search.getPhotos({
       return `<img src="${i.urls.small}" />`;
     });
     console.log(getUrls);
-    maincontent.innerHTML = getUrls.join('');
-  }
+    defaultcontent.innerHTML = getUrls.join('');
+  }
 });
+
+
+let keyword = "";
+let page = 1;
+
+async function searchImages(){
+  keyword = searchBox.value;
+  const url = `https://api.unsplash.com/search/photos?page=${page}&query=${keyword}&client_id=${accessKey}&per_page=30`;
+
+  const response = await fetch(url);
+  const data = await response.json();
+
+  if(page === 1){
+    searchResult.innerHTML = "";
+  }
+
+  const results = data.results;
+  results.map((result) => {
+    const image = document.createElement("img");
+    image.src = result.urls.small;
+    const imageLink = document.createElement("a");
+    imageLink.href = result.links.html;
+    imageLink.target = "_blank";
+
+    imageLink.appendChild(image);
+    searchResult.appendChild(imageLink);
+  })
+  showMoreBtn.style.display = "block";
+
+}
+
+searchForm.addEventListener("submit", (e) =>{
+  e.preventDefault();
+  page = 1;
+  searchImages();
+})
+
+showMoreBtn.addEventListener("click", ()=>{
+  page++;
+  searchImages();
+})
+
+
+
+
+
+const maincontent = document.querySelector('#main');
+
+
+
 
 
