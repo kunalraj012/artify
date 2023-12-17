35339133dcadca061d81ec7b0c9eb08597e4e20f
kunalraj012 <kunalraj0631@gmail.com>
2023-12-17 22:01:55 +0530

    search bar work

--- src/js/index.js
+++ src/js/index.js
@@ -1,104 +1,14 @@
 
-// import { response } from "msw";
-import {createApi} from 'unsplash-js'
 import { createApi } from "unsplash-js";
 
-const home = document.querySelector('#main');
-const category = document.querySelectorAll('.category');
-const selected = document.querySelector('.selected');
-
-let topic = "";
-// function searchimg(){
-//   const searchinput = document.getElementById('searchimg');
-//   topic = searchinput.value;
-//   console.log(topic);
-
-//   getData();
-// }
-
-async function getData(){
-  const responseData = await fetch('https://api.unsplash.com/search/photos/?query=' + input.value + '&per_page=20&client_id=000DvTexIa5o0rfXHeKlY66ZdmBh9xqnFNIlOqmmULU');
-  const mainData = await responseData.JSON();
-  const hits = mainData.hits;
-  allimg.innerHTML = ``;
-
-  document.getElementById('img_changer').innerHTML = `<img src="${hits[1].largeimageURL}" alt = "main-post/>`;
-
-  for(let i=0;i<hits.length;i++){
-    console.log(hits[i]);
-    allimg.insertAdjacentHTML("afterbegin",
-    `<li>
-        <a href="${hits[i].largeImageURL}">
-         <span id="tags">${hits[i].Tags}</span>
-         <img src="${hits[i].previewURL}" alt="${hits[i].Tags}"/>
-        </a>
-      </li>`);
-  }
-}
-getData();
-console.log("function");
-
-// picture selection
-// const picture = [
-//   {
-//     url:
-//     `https://api.unsplash.com/search/photos/?query=' +${Photography} + '&per_page=20&client_id=000DvTexIa5o0rfXHeKlY66ZdmBh9xqnFNIlOqmmULU`,
-//     category:'Photography'
-//   },
-//   {
-//     url:
-//     `https://api.unsplash.com/search/photos/?query=' +${Animals} + '&per_page=20&client_id=000DvTexIa5o0rfXHeKlY66ZdmBh9xqnFNIlOqmmULU`,
-//     category:'Animals'
-
-//   },
-//   {
-//     url:
-//     `https://api.unsplash.com/search/photos/?query=' +${Flower} + '&per_page=20&client_id=000DvTexIa5o0rfXHeKlY66ZdmBh9xqnFNIlOqmmULU`,
-//     category:'Flower'
-
-//   },
-//   {
-//     url:
-//     `https://api.unsplash.com/search/photos/?query=' +${Nature} + '&per_page=20&client_id=000DvTexIa5o0rfXHeKlY66ZdmBh9xqnFNIlOqmmULU`,
-//     category:'Nature'
-
-//   },
-//   {
-//     url:
-//     `https://api.unsplash.com/search/photos/?query=' +${Mountain} + '&per_page=20&client_id=000DvTexIa5o0rfXHeKlY66ZdmBh9xqnFNIlOqmmULU`,
-//     category:'Mountain'
-//   },
-//   {
-//     url:
-//     `https://api.unsplash.com/search/photos/?query=' +${Daylight} + '&per_page=20&client_id=000DvTexIa5o0rfXHeKlY66ZdmBh9xqnFNIlOqmmULU`,
-//     category:'Daylight'
-
-//   },
-//   {
-//     url:
-//     `https://api.unsplash.com/search/photos/?query=' +${NightOut} + '&per_page=20&client_id=000DvTexIa5o0rfXHeKlY66ZdmBh9xqnFNIlOqmmULU`,
-//     category:'NightOut'
-    
-//   },
-//   {
-//     url:
-//     `https://api.unsplash.com/search/photos/?query=' +${Computer} + '&per_page=20&client_id=000DvTexIa5o0rfXHeKlY66ZdmBh9xqnFNIlOqmmULU`,
-//     category:'Computer'
-
-//   },
-// ];
-
-
-
-
-const maincontent = document.querySelector('#main');
+const maincontent = document.querySelector('.container');
 const unsplash = createApi({
   accessKey: '000DvTexIa5o0rfXHeKlY66ZdmBh9xqnFNIlOqmmULU',
 });
 unsplash.search.getPhotos({
-  query:'Classic Art',
+  query:'All Types',
   page:1,
-  perPage:50,
+  perPage:100,
   orientation:'portrait',
 }).then(result => {
   if (result.type === 'success') {
@@ -111,3 +21,5 @@ unsplash.search.getPhotos({
     maincontent.innerHTML = getUrls.join('');
   }
 });
+
+
